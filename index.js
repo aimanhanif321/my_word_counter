3172b1e90d2ed58cef3d010dabc6e932e03a5817
Aiman khan <147042337+aimanhanif321@users.noreply.github.com>
2024-04-23 23:47:47 +0500

    this is my simple word counter

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+//import chalk for add colors to my output
+import chalk from "chalk";
+console.log(chalk.bold.bgCyanBright("\t\tWELCOM TO WORD COUNTER"));
+console.log(chalk.yellowBright("============================================================"));
+//ask question bt using await
+const counter = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence."
+    }
+]);
+//count  words using this 
+let word = counter.sentence.trim().split(" ");
+console.log(chalk.yellowBright("============================================================"));
+console.log(" - sentences words:");
+//seprate words 
+console.log(word);
+//count words
+console.log(chalk.greenBright(`- words count : ${word.length} words`));
+console.log(chalk.yellowBright("============================================================"));
